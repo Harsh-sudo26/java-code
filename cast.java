21aaecd693e0f698852c4d0d631223a7ca5299de
Harsh-sudo26 <animeart711@gmail.com>
2025-12-06 19:35:50 +0530

    typye casting

---
+++ cast.java
@@ -0,0 +1,8 @@
+public class cast {
+ public static void main(String[] args) {
+    int myint = 224242;
+    double mydouble = myint;
+    System.out.println(mydouble);
+    System.out.println(myint);
+ }   
+}
