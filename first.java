39de9fad24fd3879e354d60889b3139f0433ebcb
Harsh-sudo26 <animeart711@gmail.com>
2025-12-06 19:31:39 +0530

    update

--- first.java
+++ first.java
@@ -1,13 +1,19 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("Welcome to new code");
+        // variable declare hree
         int a = 300;
         float b = 3.446f;
         double c = 4.44646565;
         String name = "harsh";
+        boolean test =true;
+        
+            // variable print 
+            
         System.out.println("the output is "+ a);
         System.out.println("the output is "+ b);
         System.out.println("the output is "+ c);
         System.out.println("the output is "+name);
+        System.out.println("the output is "+ test);
     }
 }
